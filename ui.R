085342d06f7849e39e5fb823aacf3f970510feeb
Jeremy Yang <jeremy.z.yang@hotmail.com>
2014-12-19 03:43:44 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -33,7 +33,7 @@ shinyUI(fluidPage(
            plotOutput('plot3')),
     column(2,h5("Republican Speech:")),
     column(3,
-           tableOutput('plot4'))
+           plotOutput('plot4'))
   ),
   
   hr(),
